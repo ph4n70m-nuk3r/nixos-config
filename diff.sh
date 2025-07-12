1766aff693f02e21ae44b05d3996399b79fcdaca
ph4n70m-nuk3r <74739269+ph4n70m-nuk3r@users.noreply.github.com>
2025-07-12 16:47:10 +0100

    Add gimp3; Upd gpg + diff. (#2)

--- diff.sh
+++ diff.sh
@@ -1,1 +1,4 @@
-find . -type f -name '*.nix' -exec bash -c "diff '/etc/nixos/{}' '{}'" \;
+#set -x
+COMMAND="diff $@"
+alias pf='printf'
+find . -type f -name '*.nix' -exec bash -c "printf '%s\n\n' '>>>>> {} <<<<<'  ;  ${COMMAND} /etc/nixos/{} {}  ;  printf '\n%s\n\n' '-----------------'" \;
