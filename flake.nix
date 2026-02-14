e327349e31ac74341b52b83b85b82f6764820014
ph4n70m-nuk3r <ph4n70m.nuk3r@gmail.com>
2026-02-14 08:20:51 +0000

    Update.

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   # channels to pull source/packages from.
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:nix-community/stylix/release-25.05";
+      url = "github:nix-community/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
